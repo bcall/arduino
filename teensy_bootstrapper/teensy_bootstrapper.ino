ff16b5ccbb97fe46661988bb1facdaf17aa12586
Brian Call <callbrian@gmail.com>
2015-07-17 11:06:03 -0700

    test code includes updated cmd r to recovery, but mkdir doesn't work at the command line.

--- teensy_bootstrapper/teensy_bootstrapper.ino
+++ teensy_bootstrapper/teensy_bootstrapper.ino
@@ -1,19 +1,24 @@
 void setup() {
 
   // Get into Recovery Partition
-  delay (3000);
-  
+  delay (2000);
+
+  int i;
+  for (i=0; i<50; i++) {
   Keyboard.set_modifier(MODIFIERKEY_GUI);
   Keyboard.send_now();
   Keyboard.set_key1(KEY_R);
   Keyboard.send_now();
-  delay(5000);
+  delay(100);
 
   // Release keys and wait for machine to boot
   Keyboard.set_modifier(0);
   Keyboard.set_key1(0);
   Keyboard.send_now();
-  delay(40000);
+  delay(100);
+  }
+  
+  delay(50000);
 
   // Select English as the main language
   Keyboard.set_key1(KEY_ENTER);
@@ -23,7 +28,6 @@ void setup() {
   delay(2000);
 
   // Move the cursor to the upper left corner
-  int i;
   for (i=0; i<80; i++) {
     Mouse.move(-127, -127);
     delay(5);
@@ -61,25 +65,35 @@ void setup() {
   Keyboard.send_now();
   Keyboard.set_key1(0);
   Keyboard.send_now();
-  delay (100);
+  delay (10000);
 
   Keyboard.set_key1(KEY_ENTER);
   Keyboard.send_now();
   Keyboard.set_key1(0);
   Keyboard.send_now();
-  delay (5000);
+  delay (10000);
 
-  Keyboard.print("mkdir /Volumes/Macintosh\\ HD/var/tmp/it-dept/");
-  delay (100);
+  Keyboard.print("mkdir -p /Volumes/Macintosh\\ HD/var/tmp/it-dept && cd $_");
+  delay (10000);
 
-   // Hit return
+  // Hit return
   Keyboard.set_key1(KEY_ENTER);
   Keyboard.send_now();
   Keyboard.set_key1(0);
   Keyboard.send_now();
-  delay (100);
+  delay (10000);
 
-  Keyboard.print("/Volumes/Macintosh\\ HD/usr/bin/curl -L https://raw.githubusercontent.com/callbrian/osx_provision/master/recovery_bootstrap.sh > /Volumes/Macintosh\\ HD/var/tmp/it-dept/recovery_bootstrap.sh");
+//  Keyboard.print("cd /Volumes/Macintosh\\ HD/var/tmp/it-dept");
+//  delay (10000);
+//
+//  // Hit return
+//  Keyboard.set_key1(KEY_ENTER);
+//  Keyboard.send_now();
+//  Keyboard.set_key1(0);
+//  Keyboard.send_now();
+//  delay (200);
+
+  Keyboard.print("/Volumes/Macintosh\\ HD/usr/bin/curl -L https://raw.githubusercontent.com/callbrian/osx_provision/master/recovery_bootstrap.sh | bash");
   delay (100);
   
   Keyboard.set_key1(KEY_ENTER);
@@ -88,14 +102,14 @@ void setup() {
   Keyboard.send_now();
   delay (100);
 
-  Keyboard.print("bash /Volumes/Macintosh\\ HD/var/tmp/it-dept/recovery_bootstrap.sh");
-  delay (100);
-  
-  Keyboard.set_key1(KEY_ENTER);
-  Keyboard.send_now();
-  Keyboard.set_key1(0);
-  Keyboard.send_now();
-  
+//  Keyboard.print("bash /Volumes/Macintosh\\ HD/var/tmp/it-dept/recovery_bootstrap.sh");
+//  delay (100);
+//  
+//  Keyboard.set_key1(KEY_ENTER);
+//  Keyboard.send_now();
+//  Keyboard.set_key1(0);
+//  Keyboard.send_now();
+//  
   return;
 }
 
