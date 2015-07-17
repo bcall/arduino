d478dadc6301385b42293f730476b0ad0af8998d
Brian Call <callbrian@gmail.com>
2015-07-16 18:41:30 -0700

    cleaned up the delays

--- teensy_bootstrapper/teensy_bootstrapper.ino
+++ teensy_bootstrapper/teensy_bootstrapper.ino
@@ -7,80 +7,85 @@ void setup() {
   Keyboard.send_now();
   Keyboard.set_key1(KEY_R);
   Keyboard.send_now();
-  
   delay(5000);
 
+  // Release keys and wait for machine to boot
   Keyboard.set_modifier(0);
   Keyboard.set_key1(0);
   Keyboard.send_now();
-
-  //Wait for Boot
   delay(40000);
 
-  //Select English as the main language
+  // Select English as the main language
   Keyboard.set_key1(KEY_ENTER);
   Keyboard.send_now();
   Keyboard.set_key1(0);
   Keyboard.send_now();
-
   delay(2000);
 
-  //Move the cursor to the upper left corner
+  // Move the cursor to the upper left corner
   int i;
   for (i=0; i<80; i++) {
     Mouse.move(-127, -127);
     delay(5);
   }
-
-  delay (200);
+  delay (250);
 
   Mouse.click();
-
-  delay (50);
-
-  Keyboard.set_key1(KEY_TAB);
-  Keyboard.send_now();
-  Keyboard.set_key1(0);
-  Keyboard.send_now();
-
-  delay (50);
+  delay (250);
 
   Keyboard.set_key1(KEY_TAB);
   Keyboard.send_now();
   Keyboard.set_key1(0);
   Keyboard.send_now();
-
-  delay (50);
-
-  Keyboard.set_key1(KEY_TAB);
-  Keyboard.send_now();
-  Keyboard.set_key1(0);
-  Keyboard.send_now();
-
-  delay (50);
+  delay (100);
 
   Keyboard.set_key1(KEY_TAB);
   Keyboard.send_now();
   Keyboard.set_key1(0);
   Keyboard.send_now();
+  delay (100);
 
-  delay (50);
+  Keyboard.set_key1(KEY_TAB);
+  Keyboard.send_now();
+  Keyboard.set_key1(0);
+  Keyboard.send_now();
+  delay (100);
+
+  Keyboard.set_key1(KEY_TAB);
+  Keyboard.send_now();
+  Keyboard.set_key1(0);
+  Keyboard.send_now();
+  delay (100);
 
   Keyboard.set_key1(KEY_T);
   Keyboard.send_now();
   Keyboard.set_key1(0);
   Keyboard.send_now();
-
-  delay (50);
+  delay (100);
 
   Keyboard.set_key1(KEY_ENTER);
   Keyboard.send_now();
   Keyboard.set_key1(0);
   Keyboard.send_now();
-
   delay (5000);
 
+  Keyboard.print("mkdir /Volumes/Macintosh\\ HD/var/tmp/it-dept/");
+  delay (100);
+
+   // Hit return
+  Keyboard.set_key1(KEY_ENTER);
+  Keyboard.send_now();
+  Keyboard.set_key1(0);
+  Keyboard.send_now();
+  delay (100);
+
   Keyboard.print("/Volumes/Macintosh\\ HD/usr/bin/curl https://github.com/callbrian/osx_provision/recovery_bootstrap.sh > /Volumes/Macintosh\\ HD/var/tmp/it-dept/");
+  delay (100);
+  
+  Keyboard.set_key1(KEY_ENTER);
+  Keyboard.send_now();
+  Keyboard.set_key1(0);
+  Keyboard.send_now();
   
   return;
 }
